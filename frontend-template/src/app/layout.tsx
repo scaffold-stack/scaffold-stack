1a5998855b9aa7c9263401818a5157c805136ae8
Akinbola247 <akinbolakehinde247@gmail.com>
2026-04-10 18:50:31 +0100

    fix: ui

--- frontend-template/src/app/layout.tsx
+++ frontend-template/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react';
 import './globals.css';
 import { WalletProvider } from '../components/WalletConnect';
 import Header from '../components/Header';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'scaffold-stacks',
--- frontend-template/src/components/Footer.tsx
+++ frontend-template/src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
     return (
-    <div className='w-full my-[50px]'>         
-        <p className='text-[12px] font-mono font-500 text-[#908E8E] flex items-center justify-center text-center'>Quickly launch a full dapp on stacks. Move past the challenges posed by setup nightmare and focus on creating innovative solutions that drive progress.</p>
+    <div className='w-full mb-[50px]'>         
+        <p className='text-[12px] font-mono font-500 text-[#908E8E] flex items-center justify-center text-center'>Footer(c)</p>
     </div>
 
     );
